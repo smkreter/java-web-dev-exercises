9b9650d0f77b4ad09bfd7662b286847db33a8857
unknown <smkreter@gmail.com>
2020-08-17 19:33:42 -0500

    I think I added in the hashmap thing correctly, I guess I'll.... find out

--- src/exercises/HashMapPractice.java
+++ src/exercises/HashMapPractice.java
@@ -2,10 +2,18 @@ package exercises;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Scanner;
 
 
 public class HashMapPractice {
     public static void main(String[] args) {
-        HashMap<String, Double> students = new HashMap<>();
+        HashMap<String, Integer> students = new HashMap<>();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Type in a student name to add them to the roster:");
+        String newName = input.next();
+        System.out.println("Type in that student's ID number to associate it with the student:");
+        Integer newID = input.nextInt();
+        students.put(newName, newID);
+        System.out.println("The students roster is as follows: \n" + students);
     }
 }
